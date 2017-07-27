ed12e834353b3a43ea40e6da37bc1e5f6c7903d6
Devon Bull <dbull@live.com>
2017-07-27 17:36:39 -0600

    testing complete for now

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/final_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
 
   development: {
     client: 'pg',
