311fd1b0917039e426c044f5536528a00108ed3c
Devon Bull <dbull@live.com>
2017-07-27 16:36:06 -0600

    new api routes for orders, new knex orders table, order history loaded on page load, cart is cleared if post is successful

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -3,5 +3,8 @@ let r = express.Router()
 let models = require('./models')
 
 r.get('/inventory', models.inventory)
+r.get('/orders', models.getOrders)
+r.post('/orders', models.newOrder)
+
 
 module.exports = r 
