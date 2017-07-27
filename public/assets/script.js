091aa192400d2c17b79c4342fd3595eed4c5f3fb
Devon Bull <dbull@live.com>
2017-07-27 14:54:20 -0600

    now adding total in cart

--- public/assets/script.js
+++ public/assets/script.js
@@ -3,7 +3,11 @@ $('document').ready(() => {
   .then(res => res.json())
   .then(data => {
     cardMaker(data)
-  })
+  }).catch(err => console.log(err))
+
+  let items = localStorage.getItem('cart');
+  let parsedItems = JSON.parse(items)
+  
 })
 
 let cardMaker = (data) => {
@@ -17,18 +21,25 @@ let cardMaker = (data) => {
     let imgHtml = document.createElement('img')
     imgHtml.src = item.picture
     let priceHtml = document.createElement('p')
-    priceHtml.innerHTML += `Price: ${item.price}`
+    priceHtml.innerHTML += `Price: ${(item.price / 100).toFixed(2)}`
     let addToCartHtml = document.createElement('button')
+    addToCartHtml.classList.add('add-to-cart')
     addToCartHtml.innerHTML += 'Add to Cart'
 
     addToCartHtml.addEventListener('click', () => {
       let title = item.title
-      let price = item.price
+      let price = (item.price / 100).toFixed(2)
 
       let cart = JSON.parse(localStorage.getItem('cart')) || []
       cart.push({title: title, price: price})
       localStorage.setItem('cart', JSON.stringify(cart))
       $('#items-list').append(`<div><p>${title}</p><p>Price: ${price}</p></div>`)
+      let currentTotal = $('.cart-total').text()
+      console.log(currentTotal)
+      console.log(typeof +price)
+      let test = Number(currentTotal) + Number(price)
+      console.log(test)
+      $('.cart-total').text(test)
     })
 
     cardHtml.append(titleHtml)
