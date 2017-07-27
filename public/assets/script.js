46a4a8b6db978807a93a0027ecbf3b901ee9096e
Devon Bull <dbull@live.com>
2017-07-27 14:06:35 -0600

    now adding the item to local storage

--- public/assets/script.js
+++ public/assets/script.js
@@ -7,15 +7,47 @@ $('document').ready(() => {
 })
 
 let cardMaker = (data) => {
-  data.inventory.forEach(item => {      
-    $('#cards-wrapper').append(
-      `<div class="inventory-item">
-        <h4>${item.title}</h4>
-        <p>${item.description}</p>
-        <img src="${item.picture}"/>
-        <p>Price: ${item.price}</p>
-        <button class="add-to-cart">Add to Cart</button>
-      </div>`
-    )
+  data.inventory.forEach(item => { 
+    let cardHtml = document.createElement('div')
+    cardHtml.classList.add('inventory-item')
+    let titleHtml = document.createElement('h4')
+    titleHtml.innerHTML += item.title
+    let descriptionHtml = document.createElement('p')
+    descriptionHtml.innerHTML += item.description
+    let imgHtml = document.createElement('img')
+    imgHtml.src = item.picture
+    let priceHtml = document.createElement('p')
+    priceHtml.innerHTML += `Price: ${item.price}`
+    let addToCartHtml = document.createElement('button')
+    addToCartHtml.innerHTML += 'Add to Cart'
+
+    addToCartHtml.addEventListener('click', () => {
+      let title = item.title
+      let price = item.price
+      let cart = JSON.parse(localStorage.getItem('cart')) || []
+      cart.push({title: title, price: price})
+      localStorage.setItem('cart', JSON.stringify(cart));
+    })
+
+    cardHtml.append(titleHtml)
+    cardHtml.append(descriptionHtml)
+    cardHtml.append(imgHtml)
+    cardHtml.append(priceHtml)
+    cardHtml.append(addToCartHtml)
+    $('#cards-wrapper').append(cardHtml)
   })
 }
+
+$('#cart-reveal-button').on('click', () => {
+  console.log('fired')
+})
+
+// $('#cards-wrapper').on('click', (e) => {
+//   let item = $(e.target).parent()[0]
+//   let values = $(item).text()
+//   let actualValue = values.replace('Add to Cart', '')
+//   let test = actualValue.split('\n')
+//   let cart = JSON.parse(localStorage.getItem('cart')) || []
+//   cart.push(test)
+//   localStorage.setItem('cart', JSON.stringify(cart));
+// })
